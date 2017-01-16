d8a3101909d17b01d6c07467b1c67f91583a0a38
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-01-16 04:00:11 -0600

    edit is not working

--- public/scripts/controllers/employee.controller.js
+++ public/scripts/controllers/employee.controller.js
@@ -39,16 +39,60 @@ app.controller("EmployeeController", ["$http", function($http) {
             .success((data) => {
                 vm.employees = data;
                 getEmployees();
-                console.log("Delete Successfully");
+                console.log("Delete Successfully ");
             })
             .error((data) => {
                 console.log('Error: ', data);
             });
-
     };
 
+    // vm.editEmployee = function(id) {
+    //     console.log("edit is slicked ", id);
+    //     $http.get('/employees/' + id)
+    //         .then((req, res) => {
+    //             vm.employees = req.data;
+    //             // getEmployees();
+    //         })
+    //         .error((req, res) => {
+    //             console.log('Error in editing: ', res);
+    //         });
+    // };
+    vm.editEmployee = function(id){
+      console.log('edit function in controller ', id);
+      $http.get('/employees/' + id)
+        .then(function(req, res){
+          vm.employees = req.data;
+          //console.log(response);
+          console.log("req in client is ", req.data);
+        },
+        function(req, res){
+          console.log('Error in editing', res);
+        });
+    };
 
+    // vm.update = function(id){
+    //   console.log("update function: ", vm.contact._id);
+    //   $http.put('/contactlist/' + vm.contact._id, vm.contact)
+    //     .then(function(res, req){
+    //       refresh();
+    //     },
+    //     function(res, req){
+    //       console.log('Error in updating', res);
+    //     });
+    // };
 
+    // vm.edit = function(id){
+    //   console.log('edit function in controller ', id);
+    //   $http.get('/contactlist/' + id)
+    //     .then(function(req, res){
+    //       vm.contact = req.data;
+    //       //console.log(response);
+    //       console.log("req in client is ", req);
+    //     },
+    //     function(req, res){
+    //       console.log('Error in editing', res);
+    //     });
+    // };
 
 
 }]);
--- routes/index.js
+++ routes/index.js
@@ -74,8 +74,86 @@ router.get('/employees', (req, res, next) => {
             return res.json(results);
         });
     });
+});
+
+
+router.get('/employees/:id', (req, res, next) => {
+    const results = [];
+    // Grab data from the URL parameters
+    const id = req.params.id;
+    console.log("id in employees = ", id);
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, (err, client, done) => {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                data: err
+            });
+        }
+
+        // SELECT * FROM employees WHERE id=1;
+
+        // SQL Query > Select data
+        const query = client.query('SELECT * FROM employees WHERE id=($1)', [id]);
+        // Stream results back one row at a time
+        query.on('row', (row) => {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', () => {
+            done();
+            return res.json(results);
+        });
+    });
 
 });
+
+router.put('/employees/:id', (req, res, next) => {
+    const results = [];
+    // Grab data from the URL parameters
+    const id = req.params.id;
+
+    // Grab data from http request
+    const data = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      employee_id: req.body.employee_id,
+      salary: req.body.salary,
+      title: req.body.title,
+      active: req.body.active
+    };
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, (err, client, done) => {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                data: err
+            });
+        }
+        // SQL Query > Select data
+        client.query('UPDATE employees SET first_name=($1), last_name=($2), employee_id=($3), salary=($4), title=($5), active=($6) WHERE id=($7)', [data.first_name, data.last_name, data.employee_id, data.salary, data.title, data.active, id]);
+
+        // SQL Query > Select Data
+        const query = client.query('SELECT * FROM employees ORDER BY id ASC');
+        // Stream results back one row at a time
+        query.on('row', (row) => {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', () => {
+            done();
+            return res.json(results);
+        });
+    }); // end pg.connect
+
+}); // end route put
 
 router.delete('/employees/:id', (req, res, next) => {
     const results = [];
