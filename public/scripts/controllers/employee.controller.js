f28e0cd98b162ca76ce37fa2e905b9f95023b70c
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-01-20 01:16:29 -0600

    add port number to console

--- public/scripts/controllers/employee.controller.js
+++ public/scripts/controllers/employee.controller.js
@@ -3,14 +3,21 @@
 app.controller("EmployeeController", ["$http", "DataFactory", function($http, DataFactory) {
     console.log("EmployeeController loaded");
     var vm = this;
-    getEmployees();
-    function getEmployees() {
-        $http.get('/employees')
-            .then((response) => {
-                vm.employees = response.data;
-                vm.budgetTotal += vm.employees.salary;
-            });
-    }
+    var dataFactory = DataFactory;
+    console.log(dataFactory.currentVariable);
+
+    vm.budgetTotal = 0;
+    dataFactory.getEmployees()
+        .then((response) => {
+            vm.employees = response.data;
+            //vm.budgetTotal = vm.employees;
+            for (var i = 0; i < vm.employees.length; i++) {
+                vm.budgetTotal += vm.employees[i].salary;
+            }
+            //  console.log('response.data ', response.data[0].salary );
+            //vm.budgetTotal += vm.employees.salary;
+            //console.log("vm.budgetTotal", vm.employees.salary;
+        });
 
     // add a new employee
     vm.addEmployee = function() {
@@ -18,6 +25,7 @@ app.controller("EmployeeController", ["$http", "DataFactory", function($http, Da
         console.log("self.employee ", vm.employees);
         vm.employees.active = true;
         vm.budgetTotal += vm.employees.salary;
+        console.log("vm.employees.salary", vm.employees.salary);
         var sendObject = {
             "first_name": vm.employees.first_name,
             "last_name": vm.employees.last_name,
@@ -30,7 +38,13 @@ app.controller("EmployeeController", ["$http", "DataFactory", function($http, Da
         $http.post('/employees', sendObject)
             .then((response) => {
                     console.log('response: ', response.data);
-                    getEmployees();
+                    dataFactory.getEmployees()
+                        .then((response) => {
+                            vm.employees = response.data;
+                            //var addLength = employees.length;
+                            //console.log("addLength ", addLength);
+                            //vm.budgetTotal += vm.employees[employees.length].salary;
+                        });
                 },
                 (req, res) => {
                     console.log('Error in posting', res);
@@ -45,7 +59,11 @@ app.controller("EmployeeController", ["$http", "DataFactory", function($http, Da
         $http.delete('/employees/' + id)
             .success((data) => {
                 vm.employees = data;
-                getEmployees();
+                dataFactory.getEmployees()
+                    .then((response) => {
+                        vm.employees = response.data;
+
+                    });
                 console.log("Delete Successfully ");
             })
             .error((data) => {
--- public/scripts/services/data.factory.js
+++ public/scripts/services/data.factory.js
@@ -1,5 +1,49 @@
+/*jshint esversion: 6 */
+
 app.factory('DataFactory', ["$http", function($http) {
   console.log("DataFActory is running");
-  return 1;
+  var currentVariable = 2;
+
+  function getEmployees() {
+    //vm.budgetTotal = 0;
+    return $http.get('/employees');
+          // .then((response) => {
+          //     vm.employees = response.data;
+          //     //vm.budgetTotal += vm.employees.salary;
+          //     //console.log("vm.budgetTotal", vm.employees.salary;
+          // });
+  }
+
+
+
+  // Public API
+  // This object is what the Controllers can access
+  var publicApi = {
+    currentVariable: currentVariable,
+
+    getEmployees: function(){
+      return getEmployees();
+    }
+    // currentBudget: function() {
+    //   // return our varible to the Controller!
+    //   return currentBudget;
+    // },
+    // budgetData: function() {
+    //   // return our array to the Controller!
+    //   return budgets;
+    // },
+    // updateBudgets: function() {
+    //   // return our Promise to the Controller!
+    //   return getBudgets();
+    // },
+    // addBudget: function(newBudget) {
+    //   // return our Promise to the Controller!
+    //   var xy = addBudget(newBudget);
+    //   console.log('xy', xy);
+    //   return xy;
+    // }
+  };
+
+  return publicApi;
 
 }]);
